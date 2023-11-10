b8c75042de171b024d31f4ae25836906051fb501
Willbeckh <ndiranguwillbeckh@gmail.com>
2023-11-11 00:10:38 +0300

    remove unused imports

--- app/components/index.js
+++ app/components/index.js
@@ -2,21 +2,10 @@
 // *e.g: import {Navbar} from './components'
 
 import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Button from "./Button";
 import BreadCrumb from "./BreadCrumb";
 import ThemeToggle from "./ThemeToggle";
 import Collapsible from "./Collapsible";
 import Container from "./Container";
 import ContactForm from "./Forms/contactForm/ContactForm";
 
-export {
-  Navbar,
-  Footer,
-  Button,
-  BreadCrumb,
-  ThemeToggle,
-  Collapsible,
-  Container,
-  ContactForm,
-};
+export { Navbar, BreadCrumb, ThemeToggle, Collapsible, Container, ContactForm };
