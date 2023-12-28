8b55a995c411063ca3bfeb478bcc7257592c409f
Willbeckh <ndiranguwillbeckh@gmail.com>
2023-12-28 11:12:43 +0300

    update components export type

---
+++ components/index.js
@@ -0,0 +1,8 @@
+// *Used as: import {Navbar} from '@/components'
+
+export { default as Navbar } from "./Navbar";
+export { default as Nav } from "./Nav";
+export { default as ThemeToggle } from "./ThemeToggle";
+export { default as Collapsible } from "./Collapsible";
+export { default as Container } from "./Container";
+export { default as ContactForm } from "./Forms/contactForm/ContactForm";
