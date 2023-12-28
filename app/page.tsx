fdc57e18dfc95436f172694af72140396df90218
Willbeckh <ndiranguwillbeckh@gmail.com>
2023-12-28 23:32:01 +0300

    Update hero section content & styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
 "use client";
-import { Container } from "./components";
+import { Container } from "@/components";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-      <Container>
-        <h1>Hello,</h1>
-        <p>I am Willbeckh</p>
-        <p>Fullstack software engineer</p>
-        <p>
-          Currently working @ <span className="underline">#companyTitle</span>
-        </p>
+      <Container className={styles.homePage}>
+        <section>
+          <p className="text-sm">Hi, my name is</p>
+          <div className="">
+            <p className="text-3xl md:text-6xl font-bold text-gray-500">
+              Willbeckh
+            </p>
+            <p className="text-4xl md:text-7xl font-bold mt-6">
+              I build products for the web.
+            </p>
+
+            <p className="pt-4 md:w-3/4 font-light">
+              Specifically creating backend solutions && occasionally creating
+              frontend experiences.
+            </p>
+          </div>
+        </section>
+
+        <div className={`absolute bottom-80`}>
+          <Link
+            href="/projects"
+            className={`uppercase font-semibold text-sm text-cyan-400 ${styles.bottomNav}`}
+          >
+            View Projects
+          </Link>
+        </div>
       </Container>
     </>
   );
