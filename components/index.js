84d49fbb9162a330542996ab3cd7525c83362833
Willbeckh <ndiranguwillbeckh@gmail.com>
2023-12-30 21:39:13 +0300

    remove Navbar component

--- components/index.js
+++ components/index.js
@@ -1,6 +1,5 @@
 // *Used as: import {Navbar} from '@/components'
 
-export { default as Navbar } from "./Navbar";
 export { default as Nav } from "./Nav";
 export { default as ThemeToggle } from "./ThemeToggle";
 export { default as Collapsible } from "./Collapsible";
