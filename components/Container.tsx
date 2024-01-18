3b13c0b0d5720d1f86b4a06518a14d5fb26d216e
Willbeckh <ndiranguwillbeckh@gmail.com>
2024-01-18 13:56:26 +0300

    update wrapper container styles

--- components/Container.tsx
+++ components/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 const Container = ({ children, className }: ContainerProps) => {
   return (
-    <div className={`p-6 w-screen md:w-5/6 mx-auto md:mt-5 mb-10 ${className}`}>
+    <div className={`p-6 w-screen md:w-5/6 mx-auto md:mt-8 mb-10 ${className}`}>
       {children}
     </div>
   );
